3ac28af624e591777c2683d7fd696fe000ded236
Paul Kuttiya <w.kuttiya@gmail.com>
2017-11-02 23:57:40 -0400

    add font-awesome

--- assets/js/designs/food_order/display_cart.js
+++ assets/js/designs/food_order/display_cart.js
@@ -1,3 +1,4 @@
+//display modal
 const cart = document.querySelector("#cart-items"),
       modal = document.querySelector("#cart"),
       close = modal.querySelector(".close");
@@ -10,4 +11,23 @@ cart.addEventListener("click", (e) => {
 
 close.addEventListener("click", () => {
   modal.classList.toggle("show");
+}, false);
+
+//display list or grid
+const displayType = document.querySelector(".display-style"),
+      menuItems = document.querySelector("#menu-items");
+
+displayType.addEventListener("click", (e) => {
+  e.preventDefault();
+  e.stopPropagation();
+
+  const target = e.target.tagName.toLowerCase()
+  let className;
+
+  if (target === "i") {
+    className = e.target.closest("a").getAttribute("href").replace(/#/, '');
+    menuItems.classList = '';
+    menuItems.classList.add(className);
+  }
+
 }, false);
